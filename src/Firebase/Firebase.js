91fbf4a2c64fecebb59b5c5ead334cdd1723932a
shamim <shamimmahmud077@gmail.com>
2024-04-30 16:27:58 +0600

    added firebase config

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBliHnOgms7hcAWMzvx58VfXhidTTzqQ5U",
-  authDomain: "assignment10-41221.firebaseapp.com",
-  projectId: "assignment10-41221",
-  storageBucket: "assignment10-41221.appspot.com",
-  messagingSenderId: "382659018086",
-  appId: "1:382659018086:web:b0de01f5cbccb7393a5ca7",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
