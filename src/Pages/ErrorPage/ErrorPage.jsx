bc429ef5c6ead85bc8f88f358710fea55643bf46
shamim <shamimmahmud077@gmail.com>
2024-04-30 11:26:42 +0600

    added 404page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import { FiChevronLeft } from "react-icons/fi";
+
 const ErrorPage = () => {
   return (
     <div>
-      <h1 className="text-2xl">this is error page</h1>
+      <div className="text-center flex justify-center items-center h-[100vh]">
+        <div>
+          <img
+            className="w-[300px] h-[250px]"
+            src="https://www.ecommerce-nation.com/wp-content/uploads/2018/10/404-error.jpg.webp"
+            alt=""
+          />
+          <h2 className="text-2xl font-bold">Page Not Found</h2>
+          <Link to={"/"} className="mt-4 btn bg-[#01EEFF] flex items-center ">
+            <FiChevronLeft /> <span>Go Home </span>{" "}
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
